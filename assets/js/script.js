adcbd1d5c2838b10a826b18c982e945e6b3af3d6
Sarah Richens <sarahgrichens@gmail.com>
2023-01-24 21:39:05 -0600

    style landing page and start function for button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 //js goes here
+let findOutButtonEl = document.getElementById('find-out-btn');
+
+findOutButtonEl.addEventListener('click', nextPage);
+
+function nextPage(event) {
+    event.preventDefault();
+    console.log("the find out button is working")
+    //add class to landing page that makes it hidden
+    //add class to question mindset that makes it visible
+    //add order-first class to question mindset to push it to the top
+    //change text of header to the question and change all the styling
+
+};
