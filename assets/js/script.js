d3f1ae984b9c4f53435f312cfb7950a533929c53
Sarah Richens <sarahgrichens@gmail.com>
2023-01-26 19:57:01 -0600

    re-add previous work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,103 @@
-//js goes here
+let headerEl = document.getElementById('page-header');
+let titleEl = document.getElementById('page-title');
+let tagEl = document.getElementById('tag-line');
+let landingPageEl = document.getElementById('landing-page');
+let findOutButtonEl = document.getElementById('find-out-btn');
+let mindsetEl = document.querySelector('.question-mindset');
+let insecureEl = document.getElementById('insecure');
+let guiltEl = document.getElementById('guilt');
+let defiantEl = document.getElementById('defiant');
+let introvertedEl = document.getElementById('introverted');
+let locationQuesEl = document.getElementById('location-question');
+let locationNextEl = document.getElementById('location-next');
+let destinationQuesEl = document.getElementById('destination-question');
+let destinationNextEl = document.getElementById('destination-next');
+let timeQuesEl = document.getElementById('time-question');
+let arriveButtonEl = document.getElementById('arrive-btn');
+let answerEl = document.getElementById('answer');
+let whyButtonEl = document.getElementById('why-btn');
+let dataAnalysisEl = document.getElementById('data-analysis');
+let dataTextEl = document.getElementById('data-text');
+let refreshButtonEl = document.getElementById('refresh-btn');
+
+findOutButtonEl.addEventListener('click', mindsetPage);
+
+function mindsetPage(event) {
+    event.preventDefault();    
+    mindsetEl.classList.remove('hidden');
+    landingPageEl.classList.add('hidden');
+    tagEl.classList.add('hidden');
+    titleEl.classList.add('text-white');
+    headerEl.classList.replace('bg-blue-300', 'bg-blue-900');
+    titleEl.textContent = 'Which best describes your current mindset?';
+    titleEl.classList.replace('text-5xl', 'text-3xl');
+    titleEl.classList.remove('italic');
+    titleEl.classList.add('text-center');
+};
+
+insecureEl.addEventListener('click', locationPage);
+guiltEl.addEventListener('click', locationPage);
+defiantEl.addEventListener('click', locationPage);
+introvertedEl.addEventListener('click', intrDataPage);
+
+function locationPage(event) {
+    event.preventDefault();  
+    mindsetEl.classList.add('hidden');
+    locationQuesEl.classList.remove('hidden');
+    titleEl.textContent = 'What is your current location?';
+}
+
+function intrDataPage(event) {
+    event.preventDefault();    
+    mindsetEl.classList.add('hidden');
+    dataAnalysisEl.classList.remove('hidden');
+    titleEl.textContent = 'Data Analysis';
+    dataTextEl.textContent = "That's cool. You should stay in."
+}
+
+locationNextEl.addEventListener('click', destinationPage);
+
+function destinationPage(event) {
+    event.preventDefault();      
+    locationQuesEl.classList.add('hidden');
+    destinationQuesEl.classList.remove('hidden');
+    titleEl.textContent = 'Where are you planning on going/supposed to go?';
+}
+
+destinationNextEl.addEventListener('click', arriveTimePage);
+
+function arriveTimePage(event) {
+    event.preventDefault();     
+    destinationQuesEl.classList.add('hidden');
+    timeQuesEl.classList.remove('hidden');
+    titleEl.textContent = 'What time do you need to be there?';
+}
+
+arriveButtonEl.addEventListener('click', answerPage);
+
+function answerPage(event) {
+    event.preventDefault();        
+    timeQuesEl.classList.add('hidden');
+    answerEl.classList.remove('hidden');
+    titleEl.classList.replace('text-3xl', 'text-5xl');
+    titleEl.textContent = 'Should I go out?';
+}
+
+whyButtonEl.addEventListener('click', dataPage);
+
+function dataPage(event) {
+    event.preventDefault();     
+    answerEl.classList.add('hidden');
+    dataAnalysisEl.classList.remove('hidden');
+    titleEl.classList.remove('text-center');
+    titleEl.textContent = 'Data Analysis';
+    titleEl.classList.replace('text-5xl', 'text-3xl');   
+}
+
+refreshButtonEl.addEventListener('click', function() {
+    location.reload();
+});
+
 const apiKey = '2b53fe9e9a97281c32a772fc33b1d0b7';
 const city ='Atlanta'
 
@@ -46,24 +145,3 @@ fetch(url)
 
 
     
-let headerEl = document.getElementById('page-header');
-let titleEl = document.getElementById('page-title');
-let tagEl = document.getElementById('tag-line');
-let landingPageEl = document.getElementById('landing-page');
-let findOutButtonEl = document.getElementById('find-out-btn');
-let mindsetEl = document.querySelector('.question-mindset');
-
-findOutButtonEl.addEventListener('click', mindsetPage);
-
-function mindsetPage(event) {
-    event.preventDefault();    
-    mindsetEl.classList.remove('hidden');
-    landingPageEl.classList.add('hidden');
-    tagEl.classList.add('hidden');
-    titleEl.classList.add('text-white');
-    headerEl.classList.replace('bg-blue-300', 'bg-blue-900');
-    titleEl.textContent = 'Which best describes your current mindset?';
-    titleEl.classList.replace('text-5xl', 'text-3xl');
-    titleEl.classList.remove('italic');
-    titleEl.classList.add('text-center');
-};
